4b10968d9eb29ebc902f7db7063ff30d2d2722c5
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-01-23 22:53:49 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -1,16 +1,18 @@
-#include<stdio.h>
-void main()
+#include <stdio.h>
+int main()
 {
-char a;
-printf("enter the character");
-scanf("%c",&a);
-printf("ascii value of %c is %d ",a,a);
-if(a>=97 && a<=122 ||a>=65 && a<=90)
-{
-printf("its an alphabet");
-}
-else 
-{
-printf("its not an alphabet");
-}
+    char ch;
+    printf("Enter any character: ");
+    scanf("%c", &ch);
+    
+
+    if((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
+    {
+        printf("Character is an ALPHABET.");
+    }
+    else
+    {
+        printf("Character is NOT ALPHABET.");
+    }
+    return 0;
 }
