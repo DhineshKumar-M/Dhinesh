a93a00623b3f5807c678ee74396994dee9caf34a
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-02-11 18:26:07 +0530

    Create multiply.c

---
+++ multiply.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+int i,a[i];
+int x=1,N;
+printf("Total numbers:");
+  scanf("%d",&N);
+    printf("Enter the numbers:");
+  for(i=0;i<N;i++)
+    scanf("%d",&a[i]);
+  for(i=0;i<5;i++)
+  {
+    x=x*a[i];
+  }
+  printf("ans:%d",x);
+}
