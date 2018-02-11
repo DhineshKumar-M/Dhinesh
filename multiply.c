2176d505dd0574c36a99600640587e3cf76c9314
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-02-11 18:52:58 +0530

    Update multiply.c

--- multiply.c
+++ multiply.c
@@ -1,16 +1,12 @@
 #include<stdio.h>
 void main()
 {
-int i,a[i];
-int x=1,N;
-printf("Total numbers:");
-  scanf("%d",&N);
-    printf("Enter the numbers:");
-  for(i=0;i<N;i++)
-    scanf("%d",&a[i]);
-  for(i=0;i<5;i++)
+int i,a,x;
+printf("Enter the number:");
+  scanf("%d",&a);
+  for(i=1;i<=5;i++)
   {
-    x=x*a[i];
+    x=i*a;
+      printf("\n%d",x);
   }
-  printf("ans:%d",x);
 }
