ddd176b2f4e29614abae49128927ae83fd9fa03f
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-02-11 17:28:39 +0530

    Create sum of first n numbers.c

---
+++ sum of first n numbers.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+int i,a[i];
+int sum=0,N,k;
+printf("Total numbers:");
+  scanf("%d",&N);
+    printf("Enter the numbers:");
+  for(i=0;i<=N;i++)
+    scanf("%d",&a[i]);
+ printf("No of Numbers to be added:");
+  scanf("%d",&k);
+  for(i=0;i<k;i++)
+  {
+    sum=sum+a[i];
+  }
+  printf("%d",sum);
+}
