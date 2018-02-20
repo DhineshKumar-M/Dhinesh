eb3c870e3b59c30032a82c466cc90f5f08de4d8b
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-02-20 16:44:27 +0530

    Create max integer.c

---
+++ max integer.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int N,i,a[i],x;
+printf("enter the total integer in array:");
+scanf("%d",&N);
+printf("enter the integers");
+for(i=0;i<N;i++)
+scanf("%d",&a[i]);
+{
+  x=a[0];
+  for(i=0;i<N;i++)
+  {
+      if(a[i]>x)
+      x=a[i];
+  }
+    printf("max=%d",x);
+}
+}
