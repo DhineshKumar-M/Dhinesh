aa2651b7284001deaf8ebdab6e1de3a1c44ebd1f
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-02-22 16:57:38 +0530

    Create max in 10 integers.c

---
+++ max in 10 integers.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,a[i],x;
+printf("enter the integers");
+for(i=0;i<10;i++)
+scanf("%d",&a[i]);
+{
+  x=a[0];
+  for(i=0;i<10;i++)
+  {
+      if(a[i]>x)
+      x=a[i];
+  }
+    printf("max=%d",x);
+}
+}
