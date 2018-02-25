45116d04a4d7be337ace3b5139835f5d772793ab
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-02-25 13:52:00 +0530

    Create sorting of array elements.c

---
+++ sorting of array elements.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main() 
+{
+	int n,i,j,x,a[i];;
+	printf("\n enter the array size:");
+	scanf("%d",&n);
+	printf("\n enter the array elements:");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}
+	for (i=0;i<n;i++)
+	{
+		for(j=i+1;j<n;j++)
+		{
+			if(a[i]>a[j])
+			{
+				x=a[i];
+				a[i]=a[j];
+				a[j]=x;
+			}
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d ",a[i]);
+	}
+	return 0;
+}
