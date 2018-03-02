1c0bafbf4262211e7a41327678992ece70c63fbd
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-03-02 20:09:04 +0530

    Create check with range(1,10).c

---
+++ check with range(1,10).c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+int s;
+clrscr();
+printf("enter the number:");
+scanf("%d",&s);
+if(s>1&&s<10)
+{
+printf("YES");
+}
+else
+{
+printf("NO");
+}
+return 0;
+getch();
+}
