355708c9c84f4a0aa83f0da3865410cc3688d114
Dhineshkumarmech <35652215+Dhineshkumarmech@users.noreply.github.com>
2018-03-15 16:29:12 +0530

    Create string concatenate without strcat().c

---
+++ string concatenate without strcat().c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<conio.h>
+int main(void)
+{
+char a[20],b[20];
+clrscr();
+printf("enter the strings:");
+scanf("%s %s",a,b);
+printf("%s%s",a,b);
+getch();
+return 0;
+}
